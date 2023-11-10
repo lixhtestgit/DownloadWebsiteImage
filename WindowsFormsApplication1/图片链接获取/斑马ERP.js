bf6a54a774d8a39d4a3316cc4e58442dcb091cd0
xh li <707379913@qq.com>
2023-11-11 00:04:08 +0800

    fix: 添加斑马ERPJS

--- WindowsFormsApplication1/图片链接获取/斑马ERP.js
+++ WindowsFormsApplication1/图片链接获取/斑马ERP.js
@@ -1,1 +1,42 @@
-﻿
+﻿var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+var downLoadFunc=function(user,pageFilter,callBackFunc) {
+    pageFilter.Pager.PageNumber++;
+    pageFilter.DeveloperID.Value=user.DeveloperID;
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("filter", JSON.stringify(pageFilter));
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+  
+     fetch("https://erp.banmaerp.com/Product/Spu/SpuListData", requestOptions)
+      .then(response => response.json())
+      .then(result => callBackFunc(user,pageFilter,result));
+}
+
+var userArray=[
+    {name:"yujia",DeveloperID:32376,ImageArray:[]},
+    {name:"xuyuanyuan",DeveloperID:32373,ImageArray:[]}
+];
+var callBackFunc=function(user,pageFilter,result) {
+    if(result.Results!=null && result.Results.length>0){
+        result.Results.forEach(img=>{
+            user.ImageArray.push(img.Spu.Image);
+        });
+        downLoadFunc(user,pageFilter,callBackFunc);
+    }else{
+        console.info(JSON.stringify({name:user.name,imgs:user.ImageArray.join(",")}));
+    }
+}
+userArray.forEach(user=>{
+   var pageFilter={
+     "CreateTime":{"StartValue":"2023-11-10 00:00:00.000","EndValue":"2023-11-10 23:59:59.998","Sort":-1},
+     "Pager":{"PageNumber":0,"PageSize":10},
+     "DeveloperID":{"Value":0}
+   };
+   downLoadFunc(user,pageFilter,callBackFunc); 
+});
