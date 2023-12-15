f7c7919e1400a413ad376fdfa7c6ea712f39c6ac
xh li <707379913@qq.com>
2023-12-15 22:37:18 +0800

    fix: 更新

--- WindowsFormsApplication1/图片链接获取/斑马ERP.js
+++ WindowsFormsApplication1/图片链接获取/斑马ERP.js
@@ -17,27 +17,30 @@ var downLoadFunc = function (user, pageFilter, callBackFunc) {
         .then(response => response.json())
         .then(result => callBackFunc(user, pageFilter, result));
 }
-
+var allImageArray = [];
 var userArray = [
-    { name: "yujia", DeveloperID: 32376, ImageArray: [] },
-    { name: "xuyuanyuan", DeveloperID: 32373, ImageArray: [] },
-    { name: "jingkun", DeveloperID: 32377, ImageArray: [] },
-    { name: "gz", DeveloperID: 32472, ImageArray: [] },
-    { name: "GZ-wanying", DeveloperID: 33445, ImageArray: [] }
+    { name: "momo CPKF", DeveloperID: 32380, isDownloaded: false },
+    { name: "xuyuanyuan", DeveloperID: 32373, isDownloaded: false },
+    { name: "jingkun", DeveloperID: 32377, isDownloaded: false },
+    { name: "gz", DeveloperID: 32472, isDownloaded: false },
+    { name: "GZ-wanying02", DeveloperID: 33487, isDownloaded: false }
 ];
 var callBackFunc = function (user, pageFilter, result) {
     if (result.Results != null && result.Results.length > 0) {
         result.Results.forEach(img => {
-            user.ImageArray.push(img.Spu.Image + "?filename=" + img.Spu.Code);
+            allImageArray.push(img.Spu.Image + "?filename=" + img.Spu.Code);
         });
         downLoadFunc(user, pageFilter, callBackFunc);
     } else {
-        console.info({ name: user.name, imgs: user.ImageArray.join(",") });
+        user.isDownloaded = true;
+        if (userArray.filter(m => m.isDownloaded).length == userArray.length) {
+            console.info({ name: "all", imgs: allImageArray.join(",") });
+        }
     }
 }
 userArray.forEach(user => {
     var pageFilter = {
-        "CreateTime": { "StartValue": "2023-11-22 00:00:00.000", "EndValue": "2023-11-22 23:59:59.998", "Sort": -1 },
+        "CreateTime": { "StartValue": "2023-12-15 00:00:00.000", "EndValue": "2023-12-15 23:59:59.998", "Sort": -1 },
         "Pager": { "PageNumber": 0, "PageSize": 10 },
         "DeveloperID": { "Value": 0 }
     };
